f477f2892f27df1bf60d9e1aa74e77aaa60530b7
Morello1901 <juanmanuelmorello@gmail.com>
2024-05-03 16:29:27 -0300

    Push entrega 2

--- script.js
+++ script.js
@@ -1,24 +1,67 @@
-function Actualiza_total(id, precio) {
-    var cantidad = parseFloat(document.getElementById('cantidad_' + id).value);// Document selecciona todo el documento => cantidad_(asi llame a todos y le sumo el id para cambiar el correcto)
-    var total = cantidad * precio; //parce float lo que hace es como float en python la (str => float) y .value es tipo input y le hago lo de float porque .value te da un str
-    document.getElementById('total_' + id).innerText = '$' + total.toFixed(2);//lo mismo que antes pero con el total
-    Actualizar_total_final();//inner text me pone en una variable lo de adentro de () osea estoy reescribiendo lo que hay en total_id
-}//to fixed redonde a los decimales que les tire adentro del parentesis
+    function Actualiza_total(id, precio) {
+        var cantidad = parseFloat(document.getElementById('cantidad_' + id).value);
+        var total = cantidad * precio;
+        document.getElementById('total_' + id).innerText = '$' + total.toFixed(2);
+        Actualizar_total_final();
+    }
 
-function Actualizar_total_final() {
-    const rows = document.querySelectorAll('.product-row');// llama a todo lo que tenga .product-row y lo mete en una lista
-    let Total_final = 0;
-    rows.forEach(row => {// uso un for para recorrer la lista 
-        const totalText = row.querySelector('td[id^="total_"]').innerText;//aca recorro la fila y cuando llego al total me quedo con ese valor
-        const totalValue = parseFloat(totalText.replace('$', ''));//#inner text me da el valor que esta adentro de la id:ej: <td id="total_1">$0.00</td>#
-        Total_final += totalValue;// voy sumando los totales de cada moneda
+    function Actualizar_total_final() {
+        const rows = document.querySelectorAll('.product-row');
+        let Total_final = 0;
+        rows.forEach(row => {
+            const totalText = row.querySelector('td[id^="total_"]').innerText;
+            const valor_total_x_moneda = parseFloat(totalText.replace('$', ''));
+            Total_final += valor_total_x_moneda;
+        });
+        document.getElementById('grandTotal').innerText = '$' + Total_final.toFixed(2);
+    }
+
+    function boton_eliminar(id) {
+        var cantidadInput = document.getElementById('cantidad_' + id);
+        cantidadInput.value = 0;
+        document.getElementById('total_' + id).innerText = '$0.00';
+        Actualizar_total_final();
+    }
+    document.addEventListener('DOMContentLoaded', function() {
+        document.getElementById('btnComprar').addEventListener('click', function() {
+            Swal.fire({
+                title: "¿Deseas realizar la compra?",
+                text: "No se puede revertir la compra.",
+                icon: "warning",
+                showCancelButton: true,
+                confirmButtonColor: "#3085d6",
+                cancelButtonColor: "#d33",
+                confirmButtonText: "¡Sí!"
+            }).then((result) => {
+                if (result.isConfirmed) {
+                    Swal.fire({
+                        title: "¡Compra realizada!",
+                        icon: "success"
+                    }).then((result) => {
+                        if (result.isConfirmed) {
+                            // Redirigir al usuario al HTML principal
+                            window.location.href = "./index.html";
+                        }
+                    });
+                }
+            });
+        });
     });
-    document.getElementById('grandTotal').innerText = '$' + Total_final.toFixed(2);
-}
+    
+    // Función para verificar si el usuario está en un dispositivo móvil
+    function detectarDispositivoMovil() {
+        return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
+    }
 
-function boton_eliminar(id) {
-    var cantidadInput = document.getElementById('cantidad_' + id);
-    cantidadInput.value = 0;// vuelvo a 0 la columna de cantidad
-    document.getElementById('total_' + id).innerText = '$0.00';// mismo con el la columna de precio
-    Actualizar_total_final();//vuelve a hacer la suma con el total en 0
-}
+    // Si entras de celular te tira l< alerta y te manda para atras
+    function verificarDispositivo() {
+        if (detectarDispositivoMovil()) {
+            alert("Por motivos de seguridad, las compras solo se pueden realizar desde una computadora.");
+            window.location.href = "index.html"; // me lleva a la pagina principal
+        }
+    }
+    // Cuando se carga compras corre la funcion
+    document.addEventListener("DOMContentLoaded", function() {
+        verificarDispositivo();
+    });
+    
